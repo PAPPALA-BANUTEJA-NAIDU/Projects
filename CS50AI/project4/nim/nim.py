269a24c1e89d2e027e02eeeb4333296947f427f0
Bhanu Teja <18211a04h0@bvrit.ac.in>
2021-10-10 23:05:11 +0530

    Implemented some functions

--- CS50AI/project4/nim/nim.py
+++ CS50AI/project4/nim/nim.py
@@ -99,7 +99,10 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+        try:
+            return self.q[(tuple(state), action)]
+        except KeyError:
+            return 0
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
@@ -116,7 +119,7 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        raise NotImplementedError
+        self.q[tuple(state), action] = old_q + self.alpha * (reward + future_rewards - old_q)
 
     def best_future_reward(self, state):
         """
@@ -128,7 +131,11 @@ class NimAI():
         Q-value in `self.q`. If there are no available actions in
         `state`, return 0.
         """
-        raise NotImplementedError
+        max_reward = 0
+
+        for action in Nim.available_actions(list(state)):
+            max_reward = max(self.get_q_value(state, action), max_reward)
+        return max_reward
 
     def choose_action(self, state, epsilon=True):
         """
